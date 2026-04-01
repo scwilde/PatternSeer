9a4ca99b1a31f4260e31956450714248f4a421b1
scwilde926 <samwilde926@gmail.com>
2026-04-01 12:38:05 -0600

    Add a pannable camera

--- src/camera.rs
+++ src/camera.rs
@@ -1,27 +1,21 @@
 pub struct Camera {
-    pub x: f64,
-    pub y: f64,
-    pub width: u32,
-    pub height: u32,
+    pub x: f32,
+    pub y: f32,
     pub zoom: i32,
-    pub panning: bool,
 }
 
 impl Camera {
-    pub fn new(width: u32, height: u32) -> Self {
+    pub fn new() -> Self {
         Self {
             x: 0.0,
             y: 0.0,
-            width: width,
-            height: height,
             zoom: 20,
-            panning: false
         }
     }
 
-    pub fn pan(&mut self, delta_x: f64, delta_y: f64) {
-        self.x += delta_x;
-        self.y += delta_y;
+    pub fn pan(&mut self, delta_x: f32, delta_y: f32) {
+        self.x -= delta_x;
+        self.y -= delta_y;
         println!("Camera panned by [{}, {}]  to [{}, {}]", delta_x, delta_y, self.x, self.y);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use eframe::{egui, egui_wgpu};
+use crate::camera::Camera;
 use crate::pattern_renderer::PatternRenderer;
 use crate::utils::Volatile::{self, *};
 
 mod pattern_renderer;
 mod utils;
+mod camera;
 
 
 struct PatternSeer {
     triangles: Vec<Vec<utils::Vertex>>,
-    active_triangle: Volatile<usize>
+    active_triangle: Volatile<usize>,
+    camera: Camera,
 }
 
 impl PatternSeer {
@@ -34,35 +37,44 @@ impl PatternSeer {
                     utils::Vertex { position: [0.5, -0.5], color: [0.0, 1.0, 0.0] }
                 ],
             ],
-            active_triangle: Dirty(0)
+            active_triangle: Dirty(0),
+            camera: Camera::new(),
         }
+    }
+
+    pub fn rotate_right(&mut self) {
+        let mut active_idx = self.active_triangle.inner().clone() + 1;
+        if active_idx >= self.triangles.len() {
+            active_idx = 0;
+        }
+        self.active_triangle = Dirty(active_idx);
+    }
+    pub fn rotate_left(&mut self) {
+        let mut active_idx = self.active_triangle.inner().clone();
+        if active_idx > 0 {
+            active_idx -= 1;
+        } else {
+            active_idx = self.triangles.len() - 1;
+        }
+        self.active_triangle = Dirty(active_idx);
     }
 }
 
 impl eframe::App for PatternSeer {
     fn ui(&mut self, ui: &mut egui::Ui, frame: &mut eframe::Frame){
         egui::CentralPanel::default().show(ui, |ui| {
-            println!("New frame");
+            // println!("New frame");
 
-            if ui.input(|i| i.key_pressed(egui::Key::ArrowRight)) {
-                let mut active_idx = self.active_triangle.inner().clone() + 1;
-                if active_idx >= self.triangles.len() {
-                    active_idx = 0;
-                }
-                self.active_triangle = Dirty(active_idx);
+            if ui.input(|i| i.key_pressed(egui::Key::ArrowRight)) { self.rotate_right() }
+            if ui.input(|i| i.key_pressed(egui::Key::ArrowLeft)) { self.rotate_left() }
+
+
+
+
+            let (canvas_rect, canvas_response) = ui.allocate_exact_size(ui.available_size(), egui::Sense::drag());
+            if canvas_response.dragged() {
+                self.camera.pan(canvas_response.drag_delta().x, canvas_response.drag_delta().y);
             }
-            if ui.input(|i| i.key_pressed(egui::Key::ArrowLeft)) {
-                let mut active_idx = self.active_triangle.inner().clone();
-                if active_idx > 0 {
-                    active_idx -= 1;
-                } else {
-                    active_idx = self.triangles.len() - 1;
-                }
-                self.active_triangle = Dirty(active_idx);
-            }
-
-
-            let (rect, _response) = ui.allocate_exact_size(ui.available_size(), egui::Sense::hover());
 
             self.active_triangle.if_dirty_clean_with(|active_index| {
                 println!("Triangle dirty; Rerendering...");
@@ -70,7 +82,7 @@ impl eframe::App for PatternSeer {
             });
 
             let render_callback = PatternRenderer::render();
-            let callback_shape = egui_wgpu::Callback::new_paint_callback(rect, render_callback);
+            let callback_shape = egui_wgpu::Callback::new_paint_callback(canvas_rect, render_callback);
 
             ui.painter().add(callback_shape);
         });
